8c9b31a88dd7a9bcdfd3c7de33680d666460f755
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-10-09 19:24:56 +0600

    authProvider added

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className="font-body">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="font-body">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
