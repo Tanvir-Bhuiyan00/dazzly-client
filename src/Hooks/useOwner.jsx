a1540495ea2df8a7e011bcce195571a1479b6107
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-10-09 20:47:11 +0600

    useOwner added

---
+++ src/Hooks/useOwner.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useOwner = () => {
+  const {user, loading} = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    
+    const {data: isOwner, isLoading: isOwnerLoading} = useQuery({
+        queryKey: ['isOwner', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/owner/${user?.email}`);
+            return res.data.owner;
+        }
+    })
+    return [isOwner, isOwnerLoading]
+};
+
+export default useOwner;
